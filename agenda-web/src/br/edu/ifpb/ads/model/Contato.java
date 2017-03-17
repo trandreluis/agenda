ebc61ea55b090dba07a689b3a9bc2d49c329d5d5
Andre Luis <tr.andreluis@gmail.com>
2017-03-17 19:59:18 -0300

    Criacao do pacote e do bean referente a um contato

---
+++ agenda-web/src/br/edu/ifpb/ads/model/Contato.java
@@ -0,0 +1,37 @@
+package br.edu.ifpb.ads.model;
+
+import java.util.Date;
+
+public class Contato {
+
+	private String nome;
+	private String email;
+	private String telefone;
+	private String dataNascimento;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+}
