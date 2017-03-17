c84f4a2eaeb7fd1ed70c9d340b20b1c9dad8d8ac
Andre Luis <tr.andreluis@gmail.com>
2017-03-17 20:02:10 -0300

    Alteracao do servlet para exibir as informações do formulári (form) enviado e tratado pelo servlet

--- agenda-web/src/br/edu/ifpb/ads/servlet/PrimeiroServlet.java
+++ agenda-web/src/br/edu/ifpb/ads/servlet/PrimeiroServlet.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet(name="adicionaContato", urlPatterns={"/cadastro", "/adicionaContato"})
 public class PrimeiroServlet extends HttpServlet {
 
 	@Override
@@ -17,7 +19,10 @@ public class PrimeiroServlet extends HttpServlet {
 		
 		out.println("<html>");
 		out.println("<body>");
-		out.println("Seja bem-vindo! - response by: servlet");
+		out.println("Nome: "+requisicao.getParameter("nome")+"<br/>");
+		out.println("E-mail: "+requisicao.getParameter("email")+"<br/>");
+		out.println("Telefone: "+requisicao.getParameter("telefone")+"<br/>");
+		out.println("Data de Nascimento: "+requisicao.getParameter("dataNascimento"));
 		out.println("</body>");
 		out.println("</html>");
 		
